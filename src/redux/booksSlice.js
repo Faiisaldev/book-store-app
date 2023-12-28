d35e19996b0c956decb8f5c7396bb1d049c8e367
faiisaldev <mfsm411@gmail.com>
2023-12-28 18:43:18 +0500

    updated booksSlice

---
+++ src/redux/booksSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const booksSlice = createSlice({
+  name: "books",
+  initialState: {
+    list: [],
+  },
+  reducers: {
+    addBook: (state, action) => {
+      state.list.push(action.payload);
+    },
+    removeBook: (state, action) => {
+      state.list = state.list.filter((book) => book.id !== action.payload);
+    },
+  },
+});
+
+export const { addBook, removeBook } = booksSlice.actions;
+export default booksSlice.reducer;
